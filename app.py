72de8331560bd7864361cfe98fc14b0b6c5cd906
rongdong.zhang <rongdong.zhang@idreamsky.com>
2024-11-19 19:21:11 +0800

    多张图片拼接

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import numpy as np
 
 # 去重模板匹配法
 def stitch2(img1, img2, ratio):
+    # 如果没有传入图片
     if img1 is None or img2 is None:
         raise FileNotFoundError
-    if img1.shape != img2.shape:
+
+    # 如果宽度不一样 直接不处理
+    if img1.shape[1] != img2.shape[1]:
         raise ValueError
 
     # 考虑到旁边滑动条影响 左右个去处20像素进行匹配
@@ -13,6 +16,14 @@ def stitch2(img1, img2, ratio):
     img2 = img2[0:img2.shape[0], 20:img2.shape[1] - 20, 0:img2.shape[2]]
     cv2.imwrite("result/去掉左右20像素的图一.jpg", img1)
     cv2.imwrite("result/去掉左右20像素的图二.jpg", img2)
+
+    # 保存图片的尺寸信息
+    h1, w1, c1 = img1.shape
+    h2, w2, c2 = img2.shape
+    hdiff = h1-h2
+
+    # 把图一高度裁剪到和图二一样
+    img1 = img1[img1.shape[0] - img2.shape[0]:img1.shape[0], 0:img1.shape[1], 0:img1.shape[2]]
 
     # 如果是色彩图像，则将图像灰度化(3就是色彩图像)
     if img1.shape[2] == 3:
@@ -24,8 +35,7 @@ def stitch2(img1, img2, ratio):
 
     cv2.imwrite("result/灰度处理后的图一.jpg", gray1)
     cv2.imwrite("result/灰度处理后的图二.jpg", gray2)
-
-    h, w = gray1.shape
+    
 
     # 使用了 OpenCV 中的 自适应阈值处理，将灰度图像 gray 转换为 二值图像（即像素值为 0 或 255 的图像）
     # 这里为 255，所以像素值只有两种可能：0 和 255
@@ -39,8 +49,7 @@ def stitch2(img1, img2, ratio):
     cv2.imwrite("result/自适应阈值后的图1.jpg", thresh1)
     cv2.imwrite("result/自适应阈值后的图2.jpg", thresh2)
 
-    # 第一步 找到两张图片顶部相同的部分(也就是手机顶部的刘海、电量显示、信号等内容)
-    height = h
+    # 第一步 找到两张图片底部相同的部分 (比如微信的聊天聊天输入框)
     # 计算两幅图像的二值化差异 (相同的地方减出来肯定是0)
     sub = thresh1 - thresh2
     # 消除噪声，平滑图像
@@ -52,13 +61,14 @@ def stitch2(img1, img2, ratio):
 
     # 定义一个差异化的阈值 至少差异化要大于十分之一
     # 比如图片宽度为1000那么不同的像素至少要大于100个
-    thresh = w // 10
+    thresh = w1 // 10
 
     # 考虑到部分机型底部刘海问题 或者有滑动条等问题 去除掉底部3%的位置开始匹配
-    min_height = h*0.97
+    height = h2
+    min_height = h2*0.97
 
     # 从下到上对比(h越大也就是越底)
-    for i in range(h - 1, 0, -1):
+    for i in range(h2 - 1, 0, -1):
         # 判断当前行是否满足差异化 也就是10%的差异化
         # 并且高度必须小于97%(也就是去掉顶部的)
         if np.sum(sub[i]) > thresh and height < min_height:
@@ -92,42 +102,65 @@ def stitch2(img1, img2, ratio):
     if mn_val < 0.05:
         # result, bottom, top
         # result, 图一底部需要裁掉的部分, 图二顶部需要裁掉的部分
-        return 1, (height - block) / h, min_loc[1] / h
+        return 1, (height - block + hdiff) / h1, min_loc[1] / h2
     else:
         return 0, 1, 0
 
 
 def draw(img1, img2):
-    h, w, c = img2.shape
+    h1, w1, c1 = img1.shape
+    h2, w2, c2 = img2.shape
 
     # result, 图一底部需要裁掉的部分, 图二顶部需要裁掉的部分
     result, bottom, top = stitch2(img1, img2, 15)
 
     # 把比例转换为实际的高度
-    bottom = int(bottom * h)
-    top = int(top * h)
+    bottom = int(bottom * h1)
+    top = int(top * h2)
 
     roi1 = img1[0:bottom, ]
-    roi2 = img2[top:h, ]
+    roi2 = img2[top:h2, ]
     cv2.imwrite("result/图一裁掉剩余.jpg", roi1)
     cv2.imwrite("result/图二裁掉剩余.jpg", roi2)
 
-    # 创建一个图片 (高度为图一剩余+图二剩余)
-    image = np.ones((roi1.shape[0] + roi2.shape[0], w, 3))
+    # 创建一个图片 (高度为图一剩余+图二剩余)，显式指定 dtype=np.uint8
+    image = np.ones((roi1.shape[0] + roi2.shape[0], w1, 3), dtype=np.uint8)
     # 图片的第一部分
-    image[0:roi1.shape[0], 0:w, 0:3] = roi1
+    image[0:roi1.shape[0], 0:w1, 0:3] = roi1
     # 图片的第二部分
-    image[roi1.shape[0]:roi1.shape[0] + roi2.shape[0], 0:w, 0:3] = roi2
+    image[roi1.shape[0]:roi1.shape[0] + roi2.shape[0], 0:w1, 0:3] = roi2
     # 返回图片
     return image
 
 
+
 if __name__ == "__main__":
-    img1 = cv2.imread("./imgs/1.jpg")
-    img2 = cv2.imread("./imgs/2.jpg")
-    dst = draw(img1, img2)
-    # img1 = cv2.imread("./imgs/IMG_2941.PNG")
-    # img2 = cv2.imread("./imgs/IMG_2942.PNG")
+    import os
+    if not os.path.exists("result"):
+        os.makedirs("result")
+
+    img1 = cv2.imread("./imgs/IMG_1.PNG")
+    img2 = cv2.imread("./imgs/IMG_2.PNG")
+    img3 = cv2.imread("./imgs/IMG_3.PNG")
+    img4 = cv2.imread("./imgs/IMG_4.PNG")
+    img5 = cv2.imread("./imgs/IMG_5.PNG")
+
+    dst = None;
+    for img in [img1, img2, img3, img4, img5]:
+        if dst is None:
+            dst = img
+        else:
+            dst = draw(dst, img)   
+
+    cv2.imwrite("result/result.jpg", dst)
+
     # dst = draw(img1, img2)
-    cv2.imwrite("result/1.jpg", dst)
+    # # cv2.imwrite("result/result.jpg", dst)
+    # dst = draw(dst, img3)
+    # # cv2.imwrite("result/result.jpg", dst)
+
+    # dst = draw(dst, img4)
+    # dst = draw(dst, img5)
+    # cv2.imwrite("result/result.jpg", dst)
+
 
